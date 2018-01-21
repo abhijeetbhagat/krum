0d017e1c43b6c1aa71b62fb818b09d03b77147d0
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2018-01-22 00:48:22 +0530

    Accept source in the lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,17 +1,41 @@
 use tokens::Token;
 
-pub struct Lexer {
+pub struct Lexer<'a> {
+    src : &'a str,
     cur_tok : Token,
     cur_tok_line : usize,
     cur_tok_col : usize,
+    i : usize,
+    src_len : usize
 }
 
-impl Lexer { 
-    fn new() -> Self { 
+impl<'a> Lexer<'a> { 
+    pub fn new(src : &'a str) -> Self { 
         Lexer {
+            src,
             cur_tok : Token::Eof,
             cur_tok_line : 0,
             cur_tok_col : 0,
+            i : 0,
+            src_len : src.len()
         }
     }
+
+    pub fn get_cur_tok(&self) -> Token { 
+        self.cur_tok.clone()
+    }
+
+    pub fn get_tok(&self) -> Token {
+
+    }
+
+    fn read_and_advance(&mut self) -> Token {
+
+    }
+}
+
+#[test]
+fn test_token_read_1() { 
+    let l = Lexer::new("");
+    assert_eq!(l.get_cur_tok(), Token::Eof);
 }
--- src/tokens.rs
+++ src/tokens.rs
@@ -1,3 +1,4 @@
+#[derive(Clone, Debug, PartialEq)]
 pub enum Token {
     Ident,
     Bool,
